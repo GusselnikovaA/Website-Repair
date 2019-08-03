9a79f47e9081d912f364b40c22c08ecd3ef704fb
Anna <annanac1993@gmail.com>
2019-08-03 19:02:20 +0300

    Добавила закрытие окна через 5 секунд. Заменла var на let

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
-var button = document.querySelector('#button');
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close');
+let button = document.querySelector('#button');
+let modal = document.querySelector('#modal');
+let close = document.querySelector('#close');
 
 button.addEventListener('click', function() {
   modal.classList.add('modal_active');
+  setTimeout(function() {
+    modal.classList.remove('modal_active');
+  }, 5000);
 });
 
-close.addEventListener('click', function() {
+  close.addEventListener('click', function() {
   modal.classList.remove('modal_active');
 });
