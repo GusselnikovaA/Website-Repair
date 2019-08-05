7136828244bf637e3797fa74062e5457da04a7ba
Anna <annanac1993@gmail.com>
2019-08-05 08:45:40 +0300

    Копирование проекта в папку dist

---
+++ dist/js/main.js
@@ -0,0 +1,14 @@
+let button = document.querySelector('#button');
+let modal = document.querySelector('#modal');
+let close = document.querySelector('#close');
+
+button.addEventListener('click', function() {
+  modal.classList.add('modal_active');
+  setTimeout(function() {
+    modal.classList.remove('modal_active');
+  }, 5000);
+});
+
+  close.addEventListener('click', function() {
+  modal.classList.remove('modal_active');
+});
