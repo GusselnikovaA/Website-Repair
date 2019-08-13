ffe9d897f4cdcb8da71ef8a1f858455db16b1394
GusselnikovaA <annanac1993@gmail.com>
2019-08-13 13:07:32 +0300

    Усложненное задание 24 урока. настройкка цели на отправку формы яндекс метрика

---
+++ lesson_24/hard/js/offer.js
@@ -0,0 +1,44 @@
+//Обработка и отпарвка формы через технологию AJAX 
+$('#offer-form').on('submit', function(event) {
+  event.preventDefault();
+  $.ajax({
+    url: 'mail.php',
+    type: 'POST',
+    data: $(this).serialize(),
+    success: function() {
+      $('#success').addClass('offer-success__active');
+  
+      setTimeout(function() { // после нажатия на button мод окно автоматически закрывается чере 5000 мс
+        $('#success').removeClass('offer-success__active');
+      }, 3000);
+    },
+
+    error: function(jqXHR, textStatus) {
+      console.log(jqXHR + ': ' + textStatus);
+    }
+  });
+  $("input, textarea").val("");
+  // $("offer__input")[0].reset();
+});
+
+  $('#offer-close').on('click', function(){
+    $('#success').removeClass('offer-success__active');
+  });
+
+// $(document).ready(function(){
+//   let offerButton = $('#offer-button');
+//   let success = $('#success');
+//   let offerClose = $('#offer-close');
+
+//   offerButton.on('click', function(){
+//     success.addClass('offer-success__active');
+
+//     setTimeout(function() { // после нажатия на button мод окно автоматически закрывается чере 5000 мс
+//       success.removeClass('offer-success__active');
+//     }, 3000);
+//   });
+
+//   offerClose.on('click', function(){
+//     success.removeClass('offer-success__active');
+//   });
+// });
